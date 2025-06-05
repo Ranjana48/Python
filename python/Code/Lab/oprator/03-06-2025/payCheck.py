78eda034145cc389487aa670106170a341488aa0
Ranjana Kumari <code0994092@gmail.com>
2025-06-05 13:24:04 +0530

    paycheck calculation problem solution

--- python/Code/Lab/oprator/03-06-2025/payCheck.py
+++ python/Code/Lab/oprator/03-06-2025/payCheck.py
@@ -14,3 +14,22 @@ hours_worked = 45, hourly_rate = 15
 
 Expected Output:
 Total Pay: $712.5'''
+
+hours_worked=int(input("enter your working hors:"))
+if hours_worked<=40:
+  regular_wage = hours_worked*15
+  print(regular_wage)
+else:
+  extra_hours_worked = hours_worked-40
+  extra_wage = extra_hours_worked*(22.5)
+  total_wage =  (40*15) + extra_wage
+  print(total_wage)
+  
+
+
+
+
+    
+    
+
+  
