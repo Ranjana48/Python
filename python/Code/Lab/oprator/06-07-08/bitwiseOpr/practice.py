6179853d71c72fc94c82aa59f8c914d0a155514f
Ranjana Kumari <code0994092@gmail.com>
2025-06-06 14:22:19 +0530

    origin

---
+++ python/Code/Lab/oprator/06-07-08/bitwiseOpr/practice.py
@@ -0,0 +1,22 @@
+# right shift oprator
+# a = 12
+# b = a>>2
+# print(a, bin(a))
+# print(b, bin(b))
+# a = 12
+# b = a//2**2
+# print(a, bin(a))
+# print(b, bin(b))
+
+
+
+# left shift oprator
+# a = 12
+# b = a<<2
+# print(a, bin(a))
+# print(b, bin(b))
+
+a=12
+b = a*2**2
+print(a, bin(a))
+print(b, bin(b))
